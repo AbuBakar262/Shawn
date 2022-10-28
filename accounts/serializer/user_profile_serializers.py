24ab7093b0844d155839264950afc3a581dabc86
babar.spyresync <babar.spyresync@gmail.com>
2022-10-28 17:11:59 +0500

    user profile retrieve and update

--- accounts/serializer/user_profile_serializers.py
+++ accounts/serializer/user_profile_serializers.py
@@ -8,3 +8,12 @@ class UserProfileUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name', 'username', 'gender', 'contact', 'instagram', 'dob', 'bio']
+
+    def validate(self, attrs):
+        if not attrs.get('first_name').isalpha():
+            raise serializers.ValidationError(_('First Name must be alphabetical'))
+        if not attrs.get('last_name').isalpha():
+            raise serializers.ValidationError(_('Last Name must be alphabetical'))
+        if User.objects.filter(username=attrs['username']).exists():
+            raise serializers.ValidationError({'username': 'Username already exists'})
+        return attrs
--- accounts/view/user_profile_view.py
+++ accounts/view/user_profile_view.py
@@ -6,6 +6,7 @@ from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 from accounts.models import User
 from accounts.serializer.signin_serializers import SigninSerializer
 from accounts.serializer.signin_serializers import UserSerializer
+from accounts.serializer.user_profile_serializers import UserProfileUpdateSerializer
 
 
 class ProfileViewSet(ModelViewSet):
@@ -15,7 +16,7 @@ class ProfileViewSet(ModelViewSet):
 
     def profile_details(self, request, *args, **kwargs):
         try:
-            user = User.objects.get(email=request.data.get('email'))
+            user = User.objects.get(id=request.data.get('id'))
             if request.user.is_superuser:
                 user_serializer = UserSerializer(user)
                 return Response(
@@ -38,6 +39,71 @@ class ProfileViewSet(ModelViewSet):
                 "status": "error",
                 "status_code": status.HTTP_400_BAD_REQUEST,
                 "message": "You are not authorized to view this user data"
+            }, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response(data={
+                "status": "error",
+                "status_code": status.HTTP_400_BAD_REQUEST,
+                "message": str(e)
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProfileUpdateViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserProfileUpdateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def profile_update(self, request, *args, **kwargs):
+        try:
+            user = request.user
+            if request.user.is_superuser:
+                user = User.objects.get(id=request.data.get('id'))
+                user_serializer = UserProfileUpdateSerializer(user, data=request.data, partial=True)
+                if not user_serializer.is_valid():
+                    return Response(
+                        data={
+                            "status": "error",
+                            "status_code": status.HTTP_400_BAD_REQUEST,
+                            "message": user_serializer.errors
+                        },
+                        status=status.HTTP_400_BAD_REQUEST
+                    )
+                user_serializer.save()
+                return Response(
+                    data={
+                        "status": "success",
+                        "status_code": status.HTTP_200_OK,
+                        "message": "User data updated successfully",
+                        "responsePayload": user_serializer.data
+                    },
+                    status=status.HTTP_200_OK
+                )
+            if request.user == user:
+                user_serializer = UserProfileUpdateSerializer(request.user, data=request.data, partial=True)
+                if not user_serializer.is_valid():
+                    return Response(
+                        data={
+                            "status": "error",
+                            "status_code": status.HTTP_400_BAD_REQUEST,
+                            "message": user_serializer.errors
+                        },
+                        status=status.HTTP_400_BAD_REQUEST
+                    )
+                user_serializer.save()
+                user_serializer = UserSerializer(request.user)
+                return Response(
+                    data={
+                        "status": "success",
+                        "status_code": status.HTTP_200_OK,
+                        "message": "User data updated successfully",
+                        "responsePayload": user_serializer.data
+                    },
+                    status=status.HTTP_200_OK
+                )
+            return Response(data={
+                "status": "error",
+                "status_code": status.HTTP_400_BAD_REQUEST,
+                "message": "You are not authorized to update this user data"
             }, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response(data={

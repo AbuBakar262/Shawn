443761dff244202e0d57348de2908e2a0a9d7c72
aliraza-ar <ali.raza@spyresync.com>
2022-11-08 12:43:01 +0500

    static

--- sean_backend/settings.py
+++ sean_backend/settings.py
@@ -192,6 +192,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'us2.smtp.mailhostbox.com'

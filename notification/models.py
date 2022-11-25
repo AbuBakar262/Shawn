8a09c3e7220dd6f70aafb89eac26f7e9b5626c10
aliraza-ar <ali.raza@spyresync.com>
2022-11-25 11:20:52 +0500

    notification model

--- notification/models.py
+++ notification/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from accounts.models import *
 # Create your models here.
 
+NOTIFICATION_TYPE = (
+    ('Add Friend', 'Add Friend'),
+    ('Send Request', 'Send Request'),
+    ('Accept Request', 'Accept Request'),
+    ('Send Message', 'Send Message')
+)
+
 
 class DeviceRegistration(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_device', null=True, blank=True)
@@ -12,3 +19,14 @@ class DeviceRegistration(models.Model):
 
     def __str__(self):
         return f'{self.user} - {self.registration_id}'
+
+
+class Notification(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender_in_notification')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver_in_notification')
+    message_title = models.CharField(max_length=250, null=True, blank=True)
+    message_body = models.CharField(max_length=250, null=True, blank=True)
+    type = models.CharField(max_length=20, choices=NOTIFICATION_TYPE, null=True, blank=True)
+    read_status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)

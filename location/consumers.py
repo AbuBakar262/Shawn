a7b41d20cca6d21f3992a9f87fc611203c36a668
aliraza-ar <ali.raza@spyresync.com>
2022-12-02 18:01:52 +0500

    changes

--- location/consumers.py
+++ location/consumers.py
@@ -13,83 +13,86 @@ def get_mongodb_database():
     # Create the database for our example (we will use the same database throughout the tutorial
     return client[MONGODB_NAME]
 def update_location(latitude, longitude, user_scope):
+
     print("start function")
-    try:
-        ###############################
-        from pymongo import GEO2D
-        from rest_framework.response import Response
-        from friends_management.models import Friend
-        from rest_framework_simplejwt.tokens import AccessToken
-        from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
-        from jwt import decode as jwt_decode
-        from django.conf import settings
-        from accounts.models import User
-        user_token = dict(user_scope['headers'])[b'token'].decode("utf8")
-        user_scope["query_string"] = "token=" + user_token
-
-        token = user_token
-
-        # Try to authenticate the user
-        try:
-            # This will automatically validate the token and raise an error if token is invalid
-            AccessToken(token)
-        except (InvalidToken, TokenError) as e:
-            # Token is invalid
-            return None
-        else:
-            #  Then token is valid, decode it
-            decoded_data = jwt_decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-
-            user = decoded_data["user_id"]
-            user_scope['user'] = user
-            print("==============user_id", user)
+    # try:
         ##############################
-        if User.objects.filter(id=user).exists():
-            user_data = User.objects.filter(id=user).first()
-            if user_data.profile_thumbnail:
-                profile_thumbnail = user_data.profile_thumbnail.url
-            else:
-                profile_thumbnail = ""
-            user_friend = Friend.objects.filter(user_id=user_data.id).values_list("friend_id", flat=True)
-            friend_user = Friend.objects.filter(friend_id=user_data.id).values_list("user_id", flat=True)
-            friends_list = list(user_friend) + (list(friend_user))
-            latitude = latitude
-            longitude = longitude
-            dbname = get_mongodb_database()
-            collection_name = dbname["TestCollection"]
-            list_index_check = list(collection_name.list_indexes())
-            if len(list_index_check) == 0:
-                collection_name.create_index([("TestCollection", GEO2D)])
-            index_check = collection_name.index_information()
-            if index_check:
-                get_index = index_check.get('TestCollection_2d')
-                if get_index is None:
-                    collection_name.create_index([("TestCollection", GEO2D)])
 
-            user_found = collection_name.find({"user_id": user})
-            user_in_db = list(user_found)
-            print("============== before if condition")
-            if len(user_in_db) == 0:
-                collection_name.insert_one({"user_id": user, "profile_thumbnail": profile_thumbnail,
-                                            "friend_ids": friends_list,
-                                            "location": [float(latitude), float(longitude)]})
-            else:
-                print("before query")
-                myquery = user_in_db[0]
-                new_values = {"$set": {"user_id": user, "profile_thumbnail": profile_thumbnail,
-                                       "friend_ids": friends_list,
-                                       "location": [float(latitude), float(longitude)]}}
-                result = collection_name.update_one(myquery, new_values)
-                print("after query")
-                print("done", result.acknowledged)
-                print("end function")
+    # from rest_framework.response import Response
+    from rest_framework_simplejwt.tokens import AccessToken
+    from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+    from jwt import decode as jwt_decode
+    from django.conf import settings
 
+    user_token = dict(user_scope['headers'])[b'token'].decode("utf8")
+    user_scope["query_string"] = "token=" + user_token
+
+    token = user_token
+
+    # Try to authenticate the user
+    try:
+        # This will automatically validate the token and raise an error if token is invalid
+        AccessToken(token)
+    except (InvalidToken, TokenError) as e:
+        # Token is invalid
+        return None
+    else:
+        #  Then token is valid, decode it
+        decoded_data = jwt_decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+
+        user = decoded_data["user_id"]
+        user_scope['user'] = user
+        print("==============user_id", user)
+    ##############################
+    from accounts.models import User
+    if User.objects.filter(id=user).exists():
+        from friends_management.models import Friend
+        from pymongo import GEO2D
+        user_data = User.objects.filter(id=user).first()
+        if user_data.profile_thumbnail:
+            profile_thumbnail = user_data.profile_thumbnail.url
         else:
-            print("============== main if not working")
+            profile_thumbnail = ""
+        user_friend = Friend.objects.filter(user_id=user_data.id).values_list("friend_id", flat=True)
+        friend_user = Friend.objects.filter(friend_id=user_data.id).values_list("user_id", flat=True)
+        friends_list = list(user_friend) + (list(friend_user))
+        latitude = latitude
+        longitude = longitude
+        dbname = get_mongodb_database()
+        collection_name = dbname["TestCollection"]
+        list_index_check = list(collection_name.list_indexes())
+        if len(list_index_check) == 0:
+            collection_name.create_index([("TestCollection", GEO2D)])
+        index_check = collection_name.index_information()
+        if index_check:
+            get_index = index_check.get('TestCollection_2d')
+            if get_index is None:
+                collection_name.create_index([("TestCollection", GEO2D)])
 
-    except Exception as e:
-        error = {"status": False, "message": e.args[0]}
-        return Response(error)
+        user_found = collection_name.find({"user_id": user})
+        user_in_db = list(user_found)
+        print("============== before if condition")
+        if len(user_in_db) == 0:
+            collection_name.insert_one({"user_id": user, "profile_thumbnail": profile_thumbnail,
+                                        "friend_ids": friends_list,
+                                        "location": [float(latitude), float(longitude)]})
+        else:
+            print("before query")
+            myquery = user_in_db[0]
+            new_values = {"$set": {"user_id": user, "profile_thumbnail": profile_thumbnail,
+                                   "friend_ids": friends_list,
+                                   "location": [float(latitude), float(longitude)]}}
+            result = collection_name.update_one(myquery, new_values)
+            print("after query")
+            print("done", result.acknowledged)
+            print("end function")
+
+    else:
+        print("============== main if not working")
+
+    # except Exception as e:
+    #     error = {"status": False, "message": e.args[0]}
+    #     return Response(error)
 
 class LiveTrackingConsumer(AsyncWebsocketConsumer):
     # permission_classes = [IsAuthenticated]
